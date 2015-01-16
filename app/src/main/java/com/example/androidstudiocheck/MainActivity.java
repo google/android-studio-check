819d0c52c5a1ea4f5e81ebe458a1673a670c51dd
Wayne Piekarski <waynepie@google.com>
2015-01-16 13:11:12 -0800

    Removed unused features in the default provided template

--- app/src/main/java/com/example/androidstudiocheck/MainActivity.java
+++ app/src/main/java/com/example/androidstudiocheck/MainActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -46,27 +44,5 @@ public class MainActivity extends ActionBarActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
         });
         emailText.setText("");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
